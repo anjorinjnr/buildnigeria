00f38cdfa0f08462e4e37f0bedde3b3bc585232d
Ebenezer Anjorin <tola.anjorin@gmail.com>
2015-04-15 02:49:45 -0400

    Configure API_PATH

--- client/app/app.js
+++ client/app/app.js
@@ -22,10 +22,16 @@ define(['angular',
             .controller('MainCtrl', MainCtrl)
             .controller('LoginCtrl', LoginCtrl)
             .service('authService', AuthService)
-            .constant('API_PATH', 'http://buildnigeria-service.local/');
+
 
         app.run(['$state', '$stateParams', '$location', '$rootScope', function () {
 
         }]);
+        
+        if (window.location.hostname.indexOf('buildnigeria.com.ng') >= 0) {
+            app.constant('API_PATH', 'http://api.buildnigeria.com.ng/');
+        } else {
+            app.constant('API_PATH', 'http://buildnigeria-service.local/');
+        }
         return app;
     });
